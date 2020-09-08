72b801bc5a1fe4c0b5ffebe863a55181d693cb3b
Lara Thompson <lara.r.thompson@gmail.com>
2020-09-08 16:59:34 -0700

    new icon, duplicate links, rss feed coming

--- assets/js/header.js
+++ assets/js/header.js
@@ -6,6 +6,8 @@ document.write(`\
     <a href="https://www.linkedin.com/in/lara-r-thompson" title="lara-r-thompson">\
       <img src="/assets/images/linkedin-orange.png" height=25px></a>\
     <a href="/posts.html" title="posts">\
+      <img src="/assets/images/blog-orange.png" height=25px></a>\
+    <a href="/posts.html" title="posts">\
       <img src="/assets/images/rss-orange.png" height=25px></a>\
     <a href="/resources.html" title="resources"><img src="/assets/images/books-orange.png" height=25px></a>\
 \
